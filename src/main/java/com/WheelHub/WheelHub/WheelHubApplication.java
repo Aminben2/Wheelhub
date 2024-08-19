f98bd8f17c76304c2fed5509c20908b4e432df3d
Aminben2 <aminbenomar454@gmail.com>
2024-08-19 14:39:21 +0100

    project set-up...

---
+++ src/main/java/com/WheelHub/WheelHub/WheelHubApplication.java
@@ -0,0 +1,13 @@
+package com.WheelHub.WheelHub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WheelHubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WheelHubApplication.class, args);
+	}
+
+}
